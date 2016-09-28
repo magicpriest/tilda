b63436acc1b44925017647cfab6112e06c05ccb6
Maxim Kukarin <inq@xakep.ru>
2016-09-28 19:13:49 +0500

    PoC

---
+++ config.php
@@ -0,0 +1,6 @@
+<?php
+define('DOCUMENT_ROOT', '/usr/local/var/www/tilda/project/');
+define('APIURL', 'http://api.tildacdn.info/v1/');
+define('PUBLICKEY', '');
+define('SECRETKEY', '');
+define('PROJECTID', '');
